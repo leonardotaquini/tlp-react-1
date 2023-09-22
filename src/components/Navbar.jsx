9af439cde3b1be97bb5cb7051bfadafe523bd6e0
leonardo <leonardogtaquini@gmail.com>
2023-09-22 16:30:45 -0300

    se agrego un use state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [session, setSession] = useState(false);
+
+  const handleLogin = () => {
+    setSession(!session);
+  }
+
   return (
     <>
       <nav className="navbar  bg-dark">
         <h3 className="text-white">Navbar</h3>
+        <button onClick={ handleLogin } className="btn btn-primary m-2">{ session ? "Logout" : "Login" }</button>
       </nav>
     </>
   );
